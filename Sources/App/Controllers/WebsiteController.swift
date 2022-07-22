7133249c2e48c1a60c368591c79aab4dd2fa95fa
DevOpsThinh <nguyentruongthinhvn2020@gmail.com>
2022-07-23 00:50:18 +0700

    Using the Bootstrap framework & displaying information on the page

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -9,18 +9,21 @@ struct WebsiteController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         // Register a new route at / for render the index template
         routes.get(use: indexHandler)
-        // Register a new route at / for render the terminolgy template
+        // Register a new route at /terminologies/<id>/ for render the terminolgy template
         routes.get("terminologies", ":terminologyID", use: termHandler)
+        // Register a new route at /user/<id>/ for render the user template
+        routes.get("users", ":userID", use: userHandler)
+        // Register a new route at /user/for render the allUsers template
+        routes.get("users", use: allUsersHandler)
     }
     /// A route handler: Makes a GET request to / (root path)
-    func indexHandler (_ req: Request) throws -> EventLoopFuture<View> {
+    func indexHandler (_ req: Request) -> EventLoopFuture<View> {
         Terminology.query(on: req.db).all().flatMap { terms in
-            let termsData = terms.isEmpty ? nil : terms
-            let context = IndexContext(title: "Home", terminologies: termsData)
+            let context = IndexContext(title: "Home", terminologies: terms)
             return  req.view.render("index", context)
         }
     }
-    /// A route handler: Makes a GET request to /terminologies/<id>/user
+    /// A route handler: Makes a GET request to /terminologies/<id>/
     func termHandler(_ req: Request) -> EventLoopFuture<View> {
         Terminology
             .find(req.parameters.get("terminologyID"), on: req.db)
@@ -32,15 +35,53 @@ struct WebsiteController: RouteCollection {
                 }
         }
     }
+    /// A route handler: Makes a GET request to /users/<id>/
+    func userHandler(_ req: Request) -> EventLoopFuture<View> {
+        User
+            .find(req.parameters.get("userID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { user in
+                user.$terminologies.get(on: req.db).flatMap { terms in
+                    let context = UserContext(
+                        title: user.name,
+                        user: user,
+                        terminologies: terms)
+                    return req.view.render("user", context)
+                }
+            }
+    }
+    /// A route handler: Makes a GET request to /users/
+    func allUsersHandler(_ req: Request) -> EventLoopFuture<View> {
+        User
+            .query(on: req.db)
+            .all()
+            .flatMap { users in
+                let context = AllUsersContext(
+                    title: "All Users", users: users
+                )
+                return req.view.render("allUsers", context)
+            }
+    }
 }
 
 struct IndexContext: Encodable {
     let title: String
-    let terminologies: [Terminology]?
+    let terminologies: [Terminology]
 }
 
 struct TermContext: Encodable {
     let title: String
     let terminology: Terminology
     let user: User
+}
+
+struct UserContext: Encodable {
+    let title: String
+    let user: User
+    let terminologies: [Terminology]
+}
+
+struct AllUsersContext: Encodable {
+    let title: String
+    let users: [User]
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,10 +7,12 @@ func routes(_ app: Application) throws {
     let usersController = UsersController()
     let terminologiesController = TerminologiesController()
     let categoriesController = CategoriesController()
+    let websiteController = WebsiteController()
 
     // To hook up the routes
     try app.register(collection: usersController)
     try app.register(collection: terminologiesController)
     try app.register(collection: categoriesController)
+    try app.register(collection: websiteController)
 
 }
