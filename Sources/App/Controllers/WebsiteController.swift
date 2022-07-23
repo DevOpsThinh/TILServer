301bdfe892d57eb1ff82b9d45cfab073dc4bfecb
DevOpsThinh <nguyentruongthinhvn2020@gmail.com>
2022-07-23 14:09:06 +0700

    Display our categories & handling create, edit, and delete terminologies

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -15,12 +15,29 @@ struct WebsiteController: RouteCollection {
         routes.get("users", ":userID", use: userHandler)
         // Register a new route at /user/for render the allUsers template
         routes.get("users", use: allUsersHandler)
+        // Register a new route at /categories/ for render the allCategories template
+        routes.get("categories", use: allCatesHandler)
+        // Register a new route at /categories/<id> for render the category template
+        routes.get("categories", ":categoryID", use: cateHandler)
+        // Register a new route at /terminologies/ for render the createTerminology template
+        routes.get("terminologies", "create", use: createTermHandler)
+        // Register a new route at /terminologies/ for create a terminology & render terminolgy template
+        routes.post("terminologies", "create", use: createTermPostHandler)
+        // Register a new route at /terminologies/ for render the editTerminology template
+        routes.get("terminologies", ":terminologyID", "edit", use: editTermHandler)
+        // Register a new route at /terminologies/ for edit a terminology & render the terminolgy template
+        routes.post("terminologies", ":terminologyID", "edit", use: editTermPostHandler)
+        // Register a new route at /terminologies/ for delete a terminology & render the index template
+        routes.post("terminologies", ":terminologyID", "delete", use: deleteTermHandler)
     }
     /// A route handler: Makes a GET request to / (root path)
     func indexHandler (_ req: Request) -> EventLoopFuture<View> {
-        Terminology.query(on: req.db).all().flatMap { terms in
-            let context = IndexContext(title: "Home", terminologies: terms)
-            return  req.view.render("index", context)
+        Terminology
+            .query(on: req.db)
+            .all()
+            .flatMap { terms in
+                let context = IndexContext(title: "Home", terminologies: terms)
+                return  req.view.render("index", context)
         }
     }
     /// A route handler: Makes a GET request to /terminologies/<id>/
@@ -62,6 +79,86 @@ struct WebsiteController: RouteCollection {
                 return req.view.render("allUsers", context)
             }
     }
+    /// A route handler: Makes a GET request to /categories/
+    func allCatesHandler(_ req: Request) -> EventLoopFuture<View> {
+        Category
+            .query(on: req.db)
+            .all()
+            .flatMap { cates in
+                let context = AllCatesContext(categories: cates)
+                return req.view.render("allCategories", context)
+            }
+    }
+    /// A route handler: Makes a GET request to /categories/<id>/
+    func cateHandler(_ req: Request) -> EventLoopFuture<View> {
+        Category
+            .find(req.parameters.get("categoryID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { cate in
+                cate.$terminologies.get(on: req.db).flatMap { terms in
+                    let context = CateContext(title: cate.name, category: cate, terminologies: terms)
+                    return req.view.render("category", context)
+                }
+            }
+    }
+    /// A route handler: Makes a GET request to /terminologies/
+    func createTermHandler(_ req: Request) -> EventLoopFuture<View> {
+        User
+            .query(on: req.db)
+            .all()
+            .flatMap { users in
+                let context = CreateTermContext(users: users)
+                return req.view.render("CreateTerminology", context)
+            }
+    }
+    /// A route handler: Makes a POST request to /terminologies/
+    func createTermPostHandler(_ req: Request) throws -> EventLoopFuture<Response> {
+        let data = try req.content.decode(CreateTermData.self)
+        let term = Terminology(short: data.short, long: data.long, userID: data.userID)
+        return term.save(on: req.db).flatMapThrowing {
+            guard let id = term.id else {
+                throw Abort(.internalServerError)
+            }
+            return req.redirect(to: "/terminologies/\(id)")
+        }
+    }
+    /// A route handler: Makes a GET request to /terminologies/<id>/
+    func editTermHandler(_ req: Request) -> EventLoopFuture<View> {
+        let termFuture = Terminology
+            .find(req.parameters.get("terminologyID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+        let userQuery = User.query(on: req.db).all()
+        return termFuture.and(userQuery).flatMap { term, users in
+          let context = EditTermContext(terminology: term, users: users)
+          return req.view.render("createTerminology", context)
+        }
+      }
+     /// A route handler: Makes a POST request to /terminologies/<id>/
+      func editTermPostHandler(_ req: Request) throws -> EventLoopFuture<Response> {
+        let updateData = try req.content.decode(CreateTermData.self)
+        return Terminology
+              .find(req.parameters.get("terminologyID"), on: req.db)
+              .unwrap(or: Abort(.notFound))
+              .flatMap { term in
+                  term.short = updateData.short
+                  term.long = updateData.long
+                  term.$user.id = updateData.userID
+                  guard let id = term.id else {
+                    return req.eventLoop.future(error: Abort(.internalServerError))
+                  }
+                  let redirect = req.redirect(to: "/terminologies/\(id)")
+                  return term.save(on: req.db).transform(to: redirect)
+        }
+      }
+    /// A route handler: Makes a POST request to /terminologies/<id>/
+    func deleteTermHandler(_ req: Request) -> EventLoopFuture<Response> {
+        Terminology
+            .find(req.parameters.get("terminologyID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { term in
+                term.delete(on: req.db).transform(to: req.redirect(to: "/"))
+            }
+    }
 }
 
 struct IndexContext: Encodable {
@@ -84,4 +181,27 @@ struct UserContext: Encodable {
 struct AllUsersContext: Encodable {
     let title: String
     let users: [User]
+}
+
+struct AllCatesContext: Encodable {
+    let title = "All Categories"
+    let categories: [Category]
+}
+
+struct CateContext: Encodable {
+    let title: String
+    let category: Category
+    let terminologies: [Terminology]
+}
+
+struct CreateTermContext: Encodable {
+    let title = "New Terminology"
+    let users: [User]
+}
+
+struct EditTermContext: Encodable {
+    let title = "Edit Terminology"
+    let terminology: Terminology
+    let users: [User]
+    let editing = true
 }
